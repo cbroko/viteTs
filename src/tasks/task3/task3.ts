e5c8a495a44093ee175ea346238667eaffe4797f
Christian Bernard Bouwhuis Røkke <christian.rokke@bouvet.no>
2022-09-21 14:52:21 +0200

    Task 3 draft

--- src/tasks/task3/task3.ts
+++ src/tasks/task3/task3.ts
@@ -5,27 +5,63 @@ import { setTextContentOnElement } from "../../utils";
  * Oppgave 3.1
  */
 interface Person {
-	// TODO
+	firstName: string,
+	lastName: string,
+	age: number,
+	country: string
 }
-
+//No touchy
 const idYourSelf = ({ firstName, lastName, age, country }: Person) => {
-	setTextContentOnElement("#name", `${firstName} ${lastName}`);
+	setTextContentOnElement("#person-name", `${firstName} ${lastName}`);
+	setTextContentOnElement("#person-age", `${age}`);
+	setTextContentOnElement("#person-country", `${country}`);
 };
 const personToId = {
 	// TODO
 };
-idYourSelf(personToId);
+
+//No touchy
+document.querySelector('#task31-btn')?.addEventListener("click", () => {
+	idYourSelf(personToId);
+}) 
+
+
 
 /**
  * Oppgave 3.2
  */
-interface Student {
-	// TODO
-}
-
-/**
- * Oppgave 3.3
- */
-type NTNUStudent = {
+type Student = {
 	// TODO
 };
+
+const provideStudentId = ({firstName, lastName, studentNumber}: Student) => {
+	setTextContentOnElement("#student-name", `${firstName} ${lastName}`);
+	setTextContentOnElement("#student-id", `${studentNumber}`);
+}
+//No touchy
+document.querySelector('#task33-btn')?.addEventListener("click", () => {
+	provideStudentId()
+}) 
+
+/**
+ * Oppgave 3.2
+ */
+interface Faculty {
+	
+}
+
+ interface NTNUStudent  {
+
+}
+
+
+const provideNTNUStudentId = ({lastName, faculty, studentNumber}: NTNUStudent) => {
+	setTextContentOnElement("#student-name", `${lastName}`);
+	setTextContentOnElement("#student-id", `${studentNumber}`);
+	setTextContentOnElement("#student-faculty", `${faculty.name}`);
+	setTextContentOnElement("#student-leader", `${faculty.leader.firstName} ${faculty.leader.lastName}`);
+}
+//No touchy
+document.querySelector('#task33-btn')?.addEventListener("click", () => {
+	provideNTNUStudentId()
+}) 
